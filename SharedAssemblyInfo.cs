36d3091b69f600f176c230e3023dd7f3b059312f
feihu <378917466@qq.com>
2018-05-07 21:37:18 +0800

    版本更新

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.31.59")]
-[assembly: AssemblyFileVersion("3.10.31.59")] 
+[assembly: AssemblyVersion("3.10.31.60")]
+[assembly: AssemblyFileVersion("3.10.31.60")] 
  
 
 
